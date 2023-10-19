b1c0ee7899ae5fbb81947529804adf9db90f8f43
Manohar <indukuri.k@northeastern.edu>
2023-10-18 20:25:44 -0400

    Instance update

--- __main__.py
+++ __main__.py
@@ -1,90 +1,168 @@
 import pulumi
-import pulumi_aws as aws
+from pulumi_aws import ec2
+from decouple import config
 
-# Creating a VPC
-main = aws.ec2.Vpc("pulumi01",
-    cidr_block="10.0.0.0/16",
+# Load environment variables from the .env file
+region = config("AWS_REGION")
+availability_zones = config("AWS_AVAILABILITY_ZONES").split(",")
+num_public_subnets = int(config("MY_PUBLIC_SUBNETS"))
+num_private_subnets = int(config("MY_PRIVATE_SUBNETS"))
+vpc_cidr_prefix = config("MY_VPC_CIDR_PREFIX")
+vpc_name = config("MY_VPC_NAME")
+subnet_public_name = config("MY_SUBNET_PUBLIC_NAME")
+subnet_private_name = config("MY_SUBNET_PRIVATE_NAME")
+public_route_table = config("MY_PUBLIC_ROUTE_TABLE")
+private_route_table = config("MY_PRIVATE_ROUTE_TABLE")
+public_subnet_connect = config("MY_PUBLIC_SUBNET_CONNECT")
+private_subnet_connect = config("MY_PRIVATE_SUBNET_CONNECT")
+internet_gateway = config("MY_INTERNET_GATEWAY")
+public_route = config("MY_PUBLIC_ROUTE")
+public_route_cidr_des = config("MY_PUBLIC_ROUTE_CIDR_DES")
+instance_type = config("Instance_Type")
+ami = config("AMI")
+
+# Create a VPC
+vpc = ec2.Vpc(
+    vpc_name,
+    cidr_block=vpc_cidr_prefix,
     instance_tenancy="default",
     tags={
-        "Name": "pulumi01"
-    })
-
-# Defining the availability zones used
-availability_zones = [
-    "us-east-1a",
-    "us-east-1b",
-    "us-east-1c"
-]
+        "Name": vpc_name,
+    },
+)
 
 public_subnets = []
 private_subnets = []
 
-# Creating 3 public and 3 private subnets in different availability zones
-for i in range(3):
-    subnet_name = f"pulumi-public-subnet-{i + 1}"
-    subnet = aws.ec2.Subnet(subnet_name,
-        vpc_id=main.id,
-        availability_zone=availability_zones[i],
+# Create public and private subnets
+for i in range(num_public_subnets):
+    subnet_name = f"{subnet_public_name}{i + 1}"
+    subnet = ec2.Subnet(
+        subnet_name,
+        vpc_id=vpc.id,
+        availability_zone=region+availability_zones[i % len(availability_zones)],  # Rotate AZs
         cidr_block=f"10.0.{i + 1}.0/24",
+        map_public_ip_on_launch=True,
         tags={
-            "Name": subnet_name
-        })
+            "Name": subnet_name,
+        },
+    )
     public_subnets.append(subnet)
 
-for i in range(3):
-    subnet_name = f"pulumi-private-subnet-{i + 1}"
-    subnet = aws.ec2.Subnet(subnet_name,
-        vpc_id=main.id,
-        availability_zone=availability_zones[i],
-        cidr_block=f"10.0.{i + 4}.0/24",
+for i in range(num_private_subnets):
+    subnet_name = f"{subnet_private_name}{i + 1}"
+    subnet = ec2.Subnet(
+        subnet_name,
+        vpc_id=vpc.id,
+        availability_zone=region+availability_zones[i % len(availability_zones)],  # Rotate AZs
+        cidr_block=f"10.0.{num_public_subnets + i + 1}.0/24",
         tags={
-            "Name": subnet_name
-        })
+            "Name": subnet_name,
+        },
+    )
     private_subnets.append(subnet)
 
-# Creating route tables
-public_route_table = aws.ec2.RouteTable("publicRouteTable",
-    vpc_id=main.id,
+# Create route tables
+public_route_table = ec2.RouteTable(
+    public_route_table,
+    vpc_id=vpc.id,
     tags={
-        "Name": "PublicRouteTable"
-    })
+        "Name": public_route_table,
+    },
+)
 
-private_route_table = aws.ec2.RouteTable("privateRouteTable",
-    vpc_id=main.id,
+private_route_table = ec2.RouteTable(
+    private_route_table,
+    vpc_id=vpc.id,
     tags={
-        "Name": "PrivateRouteTable"
-    })
+        "Name": private_route_table,
+    },
+)
 
 public_route_table_associations = []
 private_route_table_associations = []
 
-# Associating public subnets with the public route table
-y = 0
-for public_subnet in public_subnets:
-    association = aws.ec2.RouteTableAssociation(f"public-subnet-connect-{y + 1}",
-        subnet_id=public_subnet.id,
-        route_table_id=public_route_table.id)
-    y += 1
+# Associate public subnets with the public route table
+for i, subnet in enumerate(public_subnets):
+    association = ec2.RouteTableAssociation(
+        f"{public_subnet_connect}{i + 1}",
+        subnet_id=subnet.id,
+        route_table_id=public_route_table.id,
+    )
     public_route_table_associations.append(association)
 
-# Associating private subnets with the private route table
-k = 0
-for private_subnet in private_subnets:
-    association = aws.ec2.RouteTableAssociation(f"private-subnet-connect-{k + 1}",
-        subnet_id=private_subnet.id,
-        route_table_id=private_route_table.id)
-    k += 1
+# Associate private subnets with the private route table
+for i, subnet in enumerate(private_subnets):
+    association = ec2.RouteTableAssociation(
+        f"{private_subnet_connect}{i + 1}",
+        subnet_id=subnet.id,
+        route_table_id=private_route_table.id,
+    )
     private_route_table_associations.append(association)
 
-# Creating an Internet Gateway
-internet_gateway = aws.ec2.InternetGateway("internetGateway",
-    vpc_id=main.id,
+# Create an Internet Gateway
+internet_gateway = ec2.InternetGateway(
+    internet_gateway,
+    vpc_id=vpc.id,
     tags={
-        "Name": "pulumi-igw"
-    })
+        "Name": internet_gateway,
+    },
+)
 
-# Creating a public route in the public route table
-public_route = aws.ec2.Route("publicRoute",
+# Create a public route in the public route table
+public_route = ec2.Route(
+    public_route,
     route_table_id=public_route_table.id,
-    destination_cidr_block="0.0.0.0/0",
-    gateway_id=internet_gateway.id)
+    destination_cidr_block=public_route_cidr_des,
+    gateway_id=internet_gateway.id,
+)
+
+app_security_group = ec2.SecurityGroup(
+    "appSecurityGroup",
+    vpc_id=vpc.id,
+    ingress=[
+        ec2.SecurityGroupIngressArgs(
+            from_port=22,
+            to_port=22,
+            protocol="tcp",
+            cidr_blocks=["0.0.0.0/0"],  # Allow SSH from anywhere
+        ),
+        ec2.SecurityGroupIngressArgs(
+            from_port=80,
+            to_port=80,
+            protocol="tcp",
+            cidr_blocks=["0.0.0.0/0"],  # Allow HTTP from anywhere
+        ),
+        ec2.SecurityGroupIngressArgs(
+            from_port=443,
+            to_port=443,
+            protocol="tcp",
+            cidr_blocks=["0.0.0.0/0"],  # Allow HTTPS from anywhere
+        ),
+        # Add ingress rule for your application port here
+        ec2.SecurityGroupIngressArgs(
+            from_port=8000,
+            to_port=8000,
+            protocol="tcp",
+            cidr_blocks=["0.0.0.0/0"],
+        ),
+    ],
+    tags={
+        "Name": "appSecurityGroup",
+    },
+)
+
+ec2_instance = ec2.Instance(
+    "ec2Instance",
+    instance_type=instance_type,
+    ami=ami,  # Replace with your custom AMI ID
+    vpc_security_group_ids=[app_security_group.id],
+    subnet_id=public_subnets[0].id,  # Choose one of your public subnets
+    root_block_device=ec2.InstanceRootBlockDeviceArgs(
+        volume_size=25,
+        volume_type="gp2",
+    ),
+    tags={
+        "Name": "EC2Instance",
+    },
+)
